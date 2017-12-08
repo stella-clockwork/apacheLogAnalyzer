658c2404d51af7e2109070f7f1df7a1fe23b1f60
stella-clockwork <sigure.yggdraisll@gmail.com>
2017-12-08 23:30:22 +0900

    Change debug print
    add debug level for edbugprint functions

--- DebugPrint.java
+++ DebugPrint.java
@@ -3,14 +3,21 @@ package apacheLogAnalyzer;
 import java.util.Collection;
 
 public class DebugPrint {
-	private static final int debug_mode = 0;
-	public static void println(String str) {
-		if (debug_mode == 1)
+
+	public static final int DEBUGING = 3;
+	public static final int WARNING = 2;
+	public static final int ERROR = 1;
+	public static final int NONE = 0;
+
+	private static final int debug_mode = NONE;
+	public static void println(String str, int level) {
+		if (debug_mode >= level)
 			System.out.println(str);
 	}
 
-	public static void println(Collection c) {
-		if (debug_mode == 1)
+	public static void println(Collection c, int level) {
+		if (debug_mode >= level) {
 			System.out.println(c);
+		}
 	}
 }
